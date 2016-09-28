fb8cb0f8c4b4fa174ce56b37cc9e5f27d6f591a3
Brad <bgreen3@uoregon.edu>
2016-09-27 20:28:01 -0600

    Fully functioning and variable  background color supported

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,36 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  user_input = params['guess']
+  message = message_setter(user_input)
+  color = set_color(message)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => color}
+end
+
+def set_color(message)
+  case message
+    when "Way too high!" then "red"
+    when "Way too low!" then "red"
+    when "Too high!" then "#ffe6e6"
+    when "Too low!" then "#ffe6e6"
+    when "You got it right!" then "green"
+  end
+end
+
+def message_setter(user_input)
+  if (user_input.to_i - 5) > SECRET_NUMBER
+    "Way too high!"
+  elsif (user_input.to_i + 5) < SECRET_NUMBER
+    "Way too low!"
+  elsif user_input.to_i > SECRET_NUMBER
+    "Too high!"
+  elsif user_input.to_i < SECRET_NUMBER
+    "Too low!"
+  elsif user_input.to_i == SECRET_NUMBER
+    "You got it right!"
+  end
+
 end
